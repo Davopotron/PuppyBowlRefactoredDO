2f26480c635b24b1feca45c49f9fe070343669a7
David <davidopotowsky14@gmail.com>
2024-10-01 14:07:33 -0500

    steps 1-3

--- src/features/puppies/puppySlice.js
+++ src/features/puppies/puppySlice.js
@@ -15,7 +15,34 @@ functions for each endpoint.
 */
 
 const puppyApi = api.injectEndpoints({
-  endpoints: (build) => ({}),
+  endpoints: (build) => ({ getPuppies: build.query({
+    query: () => "puppies",
+    transformResponse: (response) => response.data,
+    transformErrorResponse: (response) => response.data.error,
+    providesTags: ["Puppy"],
+  }),
+  getPuppy: build.query({
+    query: (id) => "puppies/", //possible pt of code break
+    transformResponse: (response) => response.data,
+    transformErrorResponse: (response) => response.data.error,
+    providesTags: ["Puppy"],
+  }),
+  addPuppy: build.mutation({
+    query: (puppy) => ({
+      url: "puppies",
+      method: "POST",
+      body: puppy,
+    }),
+    invalidatesTags: ["Puppy"],
+  }),
+  deletePuppy: build.mutation({
+    query: (puppy) => ({
+      url: "puppies",
+      method: "DELETE",
+    }),
+    invalidatesTags: ["Puppy"],
+  }),
+}),
 });
 
 export const {
